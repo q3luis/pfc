4dcb2d88974b3ac1e254b6a2fb1845f87efb60a2
luispeinado <luispeinado@MacBook-Pro-de-luispeinado.local>
2016-08-12 18:56:07 +0200

    correccion

--- bin/generate_lmbd_all_sequences.py
+++ bin/generate_lmbd_all_sequences.py
@@ -19,7 +19,7 @@ sequences=["Sequence1", "Sequence2","Sequence3","Sequence4","Sequence5"]
 
 
 for seq in sequences:
-    lmdb_path= cut.generate_lmbd_dataset(base_path+seq,base_path+seq+"_240_320",240,320)
+    lmdb_path= cut.generate_lmbd_dataset(base_path+seq,base_path+seq+"_240_320","240","320")
     cut.generate_imagen_meadn(lmdb_path,means_path)
 
 
